60e7f2d26794029cfcbc88df21a233b9925eeecd
cathdxn <cathdxn@gmail.com>
2021-09-23 13:26:49 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">Dictionary App
       </header>
+      </div>
     </div>
   );
 }
